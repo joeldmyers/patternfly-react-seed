8439f0d3a9acde4195a7bc0a3bad6eb8f24c0187
Joel <joeldmyers@gmail.com>
2020-12-02 19:44:11 +0100

    testing out progress bar from PatternFly with setInterval to update it

--- src/app/Settings/Resource/ResourceSettings.tsx
+++ src/app/Settings/Resource/ResourceSettings.tsx
@@ -1,12 +1,29 @@
-import * as React from 'react';
-import { PageSection, Title } from '@patternfly/react-core';
+import React, { useState, useEffect } from 'react';
+import { PageSection, Title, Progress } from '@patternfly/react-core';
+import { number } from 'prop-types';
 
-const ResourceSettings: React.FunctionComponent = () => (
-  <PageSection>
-    <Title headingLevel="h1" size="lg">
-      Resource Settings Page Title
-    </Title>
-  </PageSection>
-);
+const ResourceSettings: React.FunctionComponent = () => {
+  const [percentDone, setPercentDone] = useState(30);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (percentDone === 100) {
+        setPercentDone(0);
+      } else {
+        setPercentDone((percentDone) => percentDone + 1);
+      }
+    }, 250);
+    return () => clearInterval(interval);
+  }, [percentDone]);
+
+  return (
+    <PageSection>
+      <Title headingLevel="h1" size="lg">
+        Resource Settings Page Title
+      </Title>
+      <Progress value={percentDone} title="Title" />
+    </PageSection>
+  );
+};
 
 export { ResourceSettings };
